9585a15fbe9b4591117fedd9b1067248cf19c216
danbi2990 <danbi2990@gmail.com>
2017-01-26 05:04:38 +0900

    from campus

--- upload_ddaddasi.py
+++ upload_ddaddasi.py
@@ -1,5 +1,74 @@
 import requests
 
+
+def format_articles(description, articles):
+    res = '<p style="text-align: justify;">{0}</p>'.format(description.replace('\u000A','<br>'))
+    res += """
+<table id="Gray_style" cellspacing="0" cellpadding="0" border="0">
+    <tbody>
+        <tr>
+            <td>
+            <p style="text-align: justify"><img alt="" src="http://www.danbinews.com/news/editor/images/smiley/msn/lightbulb.gif" />&nbsp;{0}</p>
+""".format("관련기사로 알아보기")
+    for art in articles:
+        res +="""            <p style="text-align: justify">-{0}</p>
+            <p style="text-align: justify"><a target="_blank" href="{2}">{1}</a></p>
+""".format(art[0], art[1], art[2])
+    res += """            </td>
+        </tr>
+    </tbody>
+</table>"""
+    return res.replace('\u2027','&middot;').encode('euc-kr')
+
+
+def upload_ddaddasi(uid, upw, uname, email, title, subtitle, description, articles):
+    session = requests.Session()
+
+    login_info = {'user_id': uid, 'user_pw': upw}
+    s = session.post("http://www.danbinews.com/member/login.php", login_info)
+    # print(s.cookies)
+    # print(s.text)
+
+    upload_info = {'mode': 'input', 'idxno': '', 'area': 'D', 'level': 'B', 'view_level': 'A', 'article_type': 'B',
+                   'flash_tag': '', 'recognition': 'R', 'view_recognition': 'N', 'onoff': 'F', 'serial_number': '0',
+                   'pub_date_year': '0', 'pub_date_month': '0', 'pub_date_day': '0', 'page': '0', 'pdf': 'N',
+                   'sectionCode': 'S1N10', 'subSectionCode': '', 'serialCode': '', 
+                   'user_id': uid,
+                   'user_name': uname.encode('euc-kr'), 
+                   'user_email': email,
+                   'title': title.encode('euc-kr'), 
+                   'subTitle': subtitle.encode('euc-kr'), 
+                   'summary': '',
+                   'FCKeditor1': format_articles(description, articles)}
+    r = session.post("http://www.danbinews.com/news/userArticleWrite.php", upload_info)
+    # print('called')
+
+# articles = (('조선','기사제목1','http://chosun.com'),
+#                     ('중앙','기사제목2','http://joongang.joins.com'),
+#                     ('중앙', '기사제목3', 'http://joongang.joins.com'),
+#                     ('중앙', '기사제목4', 'http://joongang.joins.com'))
+# print(format_articles("""설명
+# 
+# """
+#                       , articles))
+# 
+# upload_ddaddasi('danbi2990','Today1234',
+#                 '박진우 기자',
+#                 'danbi2990@gmail.com','제목',
+#                 '한줄요약',
+#                 '''~아마존‧알리바바
+# 
+# 등 해외 사이트''',articles)
+
+# 
+# 
+# 
+# print(r.content)
+# print("Cookie is set to:")
+# print(r.cookies.get_dict())
+# print("-----------")
+
+
 """
 login action url: "http://www.danbinews.com/news/userArticleWrite.php"
 
@@ -46,22 +115,14 @@ login action url: "http://www.danbinews.com/news/userArticleWrite.php"
 
 """
 
-session = requests.Session()
-login_info = {'user_id': 'danbi2990', 'user_pw': 'Today1234'}
-s = session.post("http://www.danbinews.com/member/login.php", login_info)
-# print(s.cookies)
-# print(s.text)
 
-upload_info = {'mode':'input', 'idxno':'', 'area':'D', 'level':'B', 'view_level':'A', 'article_type':'B', 'flash_tag':'', 'recognition':'R', 'view_recognition':'N', 'onoff':'F', 'serial_number':'0', 'pub_date_year':'0', 'pub_date_month':'0', 'pub_date_day':'0', 'page':'0', 'pdf':'N', 'sectionCode':'S1N10', 'subSectionCode':'', 'serialCode':'', 'user_id':'danbi2990','user_name':'박진우 기자'.encode('euc-kr'), 'user_email':'danbi2990@gmail.com', 'title':'ddaddasi test', 'subTitle':'ddaddasi test', 'summary':'', 'FCKeditor1':'<p>ddaddasi test</p>'}
-r = session.post("http://www.danbinews.com/news/userArticleWrite.php", upload_info)
+# print("called")
+
+# login_info = {'user_id': 'danbi2990', 'user_pw': 'Today1234'}
+# s = session.post("http://www.danbinews.com/member/login.php", login_info)
+# # print(s.cookies)
+# # print(s.text)
 # 
-# 
-# 
-# print(r.content)
-# print("Cookie is set to:")
-# print(r.cookies.get_dict())
-# print("-----------")
-
-
-
+# upload_info = {'mode':'input', 'idxno':'', 'area':'D', 'level':'B', 'view_level':'A', 'article_type':'B', 'flash_tag':'', 'recognition':'R', 'view_recognition':'N', 'onoff':'F', 'serial_number':'0', 'pub_date_year':'0', 'pub_date_month':'0', 'pub_date_day':'0', 'page':'0', 'pdf':'N', 'sectionCode':'S1N10', 'subSectionCode':'', 'serialCode':'', 'user_id':'danbi2990','user_name':'박진우 기자'.encode('euc-kr'), 'user_email':'danbi2990@gmail.com', 'title':'ddaddasi test', 'subTitle':'ddaddasi test', 'summary':'', 'FCKeditor1':'<p>ddaddasi test</p>'}
+# r = session.post("http://www.danbinews.com/news/userArticleWrite.php", upload_info)
 
--- view.py
+++ view.py
@@ -1,94 +1,126 @@
-from tkinter import *
-import edit_newspaper
+import tkinter as tk
+import webbrowser
+import edit_newspaper as news
+import upload_ddaddasi as up
+
+class MainApplication(tk.Frame):
+    def __init__(self, parent, *args, **kwargs):
+        tk.Frame.__init__(self, parent, *args, **kwargs)
+        self.parent = parent
+        self.fields_user_info = (['ID','sooahmin'], ['PW',''], ['기자이름','민수아 기자'], ['email','sooahmin09@gmail.com'])
+        self.pack(side="top", fill="both", expand=True)
+        self.e_u = [tk.StringVar() for _ in range(len(self.fields_user_info))]
+        self.frame_user_info(self.fields_user_info)  # entries from user input
+        self.e_k = tk.StringVar()
+        self.frame_ddaddasi_keyword()    # entry ddaddasi keyword
+        self.e_km = tk.StringVar()
+        self.frame_ddaddasi_meaning()   # entry ddaddasi meaning
+        self.e_kd = self.frame_ddaddasi_description()   # entry ddaddasi description
+        # self.e_kd = tk.StringVar()
+        # self.frame_ddaddasi_description()   # entry ddaddasi description
+        # print(self.e_u[2])
+
+    def frame_user_info(self, fields):
+        entries = {}
+        for i in range(len(fields)):
+            row = tk.Frame(self)
+            lab = tk.Label(row, width=10, text=fields[i][0], anchor='w')
+
+            if fields[i][0] == 'PW':
+                ent = tk.Entry(row, show='*', textvariable=self.e_u[i])
+            else:
+                ent = tk.Entry(row, textvariable=self.e_u[i])
+
+            ent.insert(0, fields[i][1])
+            if fields[i][0] == 'ID':
+                ent.focus_set()
+
+            row.pack(side=tk.TOP, fill=tk.X, padx=5, pady=5)
+            lab.pack(side=tk.LEFT)
+            ent.pack(side=tk.RIGHT, expand=tk.YES, fill=tk.X)
+
+    def frame_ddaddasi_keyword(self):
+        row = tk.Frame(self)
+        lab = tk.Label(row, width=10, text="용어", anchor='w')
+        ent = tk.Entry(row, textvariable=self.e_k)
+        butt = tk.Button(row, text='기사 검색', command=(lambda e=ent: self.frame_articles(e)))
+
+        row.pack(side=tk.TOP, fill=tk.X, padx=5, pady=5)
+        lab.pack(side=tk.LEFT)
+        ent.pack(side=tk.LEFT, expand=tk.YES, fill=tk.X)
+        butt.pack(side=tk.RIGHT, padx=5, pady=5)
+        # return ent.get()
+
+    def frame_ddaddasi_meaning(self):
+        row = tk.Frame(self)
+        lab = tk.Label(row, width=10, text="한줄요약", anchor='w')
+        ent = tk.Entry(row, textvariable=self.e_km)
+
+        row.pack(side=tk.TOP, fill=tk.X, padx=5, pady=5)
+        lab.pack(side=tk.LEFT)
+        ent.pack(side=tk.LEFT, expand=tk.YES, fill=tk.X)
+        # return ent.get()
+
+    def frame_articles(self, ent, stop=20):
+        # print(ent.get())
+        articles = news.get_articles(ent.get(), stop)
+        # articles = (('조선','기사제목1','http://chosun.com'),
+        #             ('중앙','기사제목2','http://joongang.joins.com'),
+        #             ('중앙', '기사제목3', 'http://joongang.joins.com'),
+        #             ('중앙', '기사제목4', 'http://joongang.joins.com'))
+        cnt = len(articles)
+        isChecked = [tk.IntVar() for _ in range(cnt)]
+        art = tk.Frame(self)
+        lab = tk.Label(art, width=10, text="기사", anchor='w')
+        butt = tk.Button(art, text='업로드', 
+                         command=(lambda a=articles,c=cnt,i=isChecked: self.set_article_html(a,c,i)))
+
+        for i in range(cnt):
+            cbox = tk.Checkbutton(art, text=articles[i][0]+': '+articles[i][1], variable=isChecked[i])
+            link = tk.Label(art, text=articles[i][2], fg="blue")
+            link.bind("<Button-1>", lambda event,e=articles[i][2]: self.open_url(event,e))
+            cbox.grid(column=1, row=i*2)
+            link.grid(column=1, row=i*2+1)
+            # cbox.pack(side=tk.RIGHT)
+        art.pack(side=tk.TOP, fill=tk.X, padx=5, pady=5)
+        lab.grid(column=0, row=0, sticky=tk.W)
+        butt.grid(column=0, row=1)
+        # print([x.get() for x in isChecked])
+
+    def set_article_html(self, articles, cnt, isChecked):
+        # self.fields_user_info = (['ID',''], ['PW',''], ['기자이름','ex) 박진우 기자'], ['email',''])
+        # self.e_u; self.e_k; self.e_km; self.e_kd
+        selected = [articles[x] for x in range(cnt) if isChecked[x].get() == 1]
+        up.upload_ddaddasi(self.e_u[0].get(),self.e_u[1].get(),
+                           self.e_u[2].get(),self.e_u[3].get(),
+                           self.e_k.get(),self.e_km.get(),
+                           self.e_kd.get("1.0", tk.END),selected)
+        # print([articles[x] for x in range(cnt) if isChecked[x].get() == 1])
+
+    def open_url(self, event, url):
+        webbrowser.open_new(url)
+
+    def frame_ddaddasi_description(self):
+        des = tk.Frame(self)
+        lab = tk.Label(des, width=10, height=20, text="설명", anchor='w')
+        scr = tk.Scrollbar(des)
+        # tex = tk.Entry(des, height=4, width=50, textvariable=self.e_kd)
+        tex = tk.Text(des, height=4, width=50)
+
+        scr.config(command=tex.yview)
+        tex.config(yscrollcommand=scr.set)
+
+        des.pack(side=tk.TOP, fill=tk.X, padx=5, pady=5)
+        lab.pack(side=tk.LEFT)
+        scr.pack(side=tk.RIGHT, fill=tk.Y)
+        tex.pack(side=tk.RIGHT, fill=tk.Y)
+
+        # return tex.get("1.0", tk.END)
+        return tex
 
 
-
-
-class Checkbar(Frame):
-    def __init__(self, parent=None, picks=[], side=LEFT, anchor=W):
-        Frame.__init__(self, parent)
-        self.vars = []
-        for pick in picks:
-            var = IntVar()
-            chk = Checkbutton(self, text=pick, variable=var)
-            chk.pack(side=side, anchor=anchor, expand=YES)
-            self.vars.append(var)
-
-    def state(self):
-        return map((lambda var: var.get()), self.vars)
-
-
-fields = (['ID',''], ['PW',''], ['기자이름','ex) 박진우 기자'], ['email',''])
-articles = []
-
-def makeform(root, fields):
-    entries = []
-    for field in fields:
-        row = Frame(root)
-        lab = Label(row, width=15, text=field[0], anchor='w')
-        ent = Entry(row)
-        ent.insert(10, field[1])
-        row.pack(side=TOP, fill=X, padx=5, pady=5)
-        lab.pack(side=LEFT)
-        ent.pack(side=RIGHT, expand=YES, fill=X)
-        entries.append((field[0], ent))
-    return entries
-
-
-def showArticles(frame, articles):
-    lb = Listbox(frame)
-    for i in range(len(articles)):
-        print(articles[i][0])
-        lb.insert(i, articles[i][0])
-    lb.pack(side=RIGHT, fill=X)
-    
-
-def set_articles(ent_keyword, frame, stop=50):
-    global articles
-    # print(keyword.get())
-    articles = edit_newspaper.get_articles(ent_keyword.get(), stop)
-    showArticles(frame, articles)
-
-
-if __name__ == '__main__':
-    
-    root = Tk()
+if __name__ == "__main__":
+    root = tk.Tk()
     root.title("따따시 귀찮아 죽겠네")
-
-    ents = makeform(root, fields)
-    
-    keyword = Frame(root)
-    keyword.pack(side=TOP, fill=X, padx=5, pady=5)
-    lab_keyword = Label(keyword, width=15, text="용어", anchor='w').pack(side=LEFT)
-    ent_keyword = Entry(keyword)
-    ent_keyword.pack(side=LEFT, expand=YES, fill=X)
-    # keyword_text = ent_keyword.get()
-    
-    
-    
-    description = Frame(root)
-    description.pack(side=TOP, fill=X, padx=5, pady=5)
-    lab_des = Label(description, width=15, height=20, text="설명", anchor='w').pack(side=LEFT)
-    
-    S = Scrollbar(description)
-    T = Text(description, height=4, width=50)
-    S.pack(side=RIGHT, fill=Y)
-    T.pack(side=RIGHT, fill=Y)
-    S.config(command=T.yview)
-    T.config(yscrollcommand=S.set)
-
-    # print(T.get("1.0",END))
-    
-    
-    article = Frame(root)
-    article.pack(side=TOP, fill=X, padx=5, pady=5)
-    lab_article = Label(article, width=15, text="기사", anchor='w').pack(side=LEFT)
-
-    butt_search = Button(keyword, text='기사 검색', command=(lambda e=ent_keyword, f=article: set_articles(e, f)))
-    butt_search.pack(side=RIGHT, padx=5, pady=5)
-
-    # lng = Checkbar(article, articles)
-    # lng.pack(side=TOP, fill=X)
-    # lng.config(relief=GROOVE, bd=2)
-    
+    MainApplication(root)
     root.mainloop()
