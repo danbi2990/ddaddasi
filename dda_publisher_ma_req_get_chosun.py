50f6a83225b49b1726947bede36c979bcb40c7c6
Jake Park <danbi2990@gmail.com>
2017-08-29 17:45:00 +0900

    from danbi

--- dda_publisher_ma_req_get_chosun.py
+++ dda_publisher_ma_req_get_chosun.py
@@ -4,16 +4,18 @@ from dda_publisher import Publisher
 조선
 url:
 http://search.chosun.com/search/news.search?query={}&pageno=0&orderby=&naviarraystr=&kind=&cont1=&cont2=&cont5=&categoryname=&categoryd2=&c_scope=news&sdate=&edate=&premium=
+http://search.chosun.com/search/news.search?query={}&pageno=0&orderby=&naviarraystr=&kind=&cont1=&cont2=&cont5=&categoryname=&categoryd2=&c_scope=news&sdate=&edate=&premium=
+http://search.chosun.com/search/news.search?query={}&pageno=0&orderby=news&naviarraystr=&kind=&cont1=&cont2=&cont5=&categoryname=%EC%A1%B0%EC%84%A0%EC%9D%BC%EB%B3%B4&categoryd2=&c_scope=navi&sdate=&edate=&premium=#;
 """
 
 
 class ChoSun(Publisher):
     def __init__(self):
-        super().__init__("조선일보", "http://search.chosun.com/search/news.search?query={}&pageno=0&orderby=&naviarraystr=&kind=&cont1=&cont2=&cont5=&categoryname=&categoryd2=&c_scope=news&sdate=&edate=&premium=")
+        super().__init__("조선일보", "http://search.chosun.com/search/news.search?query={}&pageno=0&orderby=news&naviarraystr=&kind=&cont1=&cont2=&cont5=&categoryname=%EC%A1%B0%EC%84%A0%EC%9D%BC%EB%B3%B4&categoryd2=&c_scope=navi&sdate=&edate=&premium=#;")
 
     def navigate_article(self, bs_obj):
         try:
-            tags = bs_obj.find("div", {"class":"result_box"}).find_all("dt")
+            tags = bs_obj.find("div", {"class":"search_news_box"}).find_all("dt")
 
             for tag in tags:
                 try:
@@ -25,7 +27,8 @@ class ChoSun(Publisher):
             pass
 
 
-# k = ChoSun()
-# k.set_keyword('김정남')
-# k.run()
-# print(k.articles)
+if __name__ == "__main__":
+    k = ChoSun()
+    k.set_keyword('이재용')
+    k.run()
+    print(k.articles)
